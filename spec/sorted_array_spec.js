ff41e27da6b47a79706d3b85d06ad08973dcbc4a
Corey Burrows <corey.burrows@gmail.com>
2012-03-30 15:27:13 -0500

    Refactors Z.SortedArray to allow sorting by a given path instead of a function.

--- spec/sorted_array_spec.js
+++ spec/sorted_array_spec.js
@@ -2,45 +2,217 @@
 
 if (typeof Z === 'undefined') { require('./helper'); }
 
-describe('Z.SortedArray constructor', function() {
-  describe('with no arguments', function() {
-    it('should set the comparison function to `Z.cmp`', function() {
-      var a = Z.SortedArray.create();
-      expect(a.cmp).toBe(Z.cmp);
-    });
+var Foo, Bar;
+
+Foo = Z.Object.extend(function() { this.property('bar'); });
+Bar = Z.Object.extend(function() { this.property('x'); });
+
+describe('Z.SortedArray constructor with default options', function() {
+  it('should insert items in sorted order as determined by `Z.cmp`', function() {
+    var a1 = Z.SortedArray.create(),
+        a2 = Z.SortedArray.create();
+
+    a1.insert(9);
+    expect(a1).toEq(Z.A(9));
+    a1.insert(3);
+    expect(a1).toEq(Z.A(3, 9));
+    a1.insert(12);
+    expect(a1).toEq(Z.A(3, 9, 12));
+    a1.insert(2);
+    expect(a1).toEq(Z.A(2, 3, 9, 12));
+
+    a2.insert('the');
+    expect(a2).toEq(Z.A('the'));
+    a2.insert('quick');
+    expect(a2).toEq(Z.A('quick', 'the'));
+    a2.insert('brown');
+    expect(a2).toEq(Z.A('brown', 'quick', 'the'));
+    a2.insert('fox');
+    expect(a2).toEq(Z.A('brown', 'fox', 'quick', 'the'));
+  });
+});
+
+describe('Z.SortedArray constructor with `isDescending` option', function() {
+  it('should insert items in sorted order as determined by `Z.cmp` but in descending order', function() {
+    var a1 = Z.SortedArray.create({isDescending: true}),
+        a2 = Z.SortedArray.create({isDescending: true});
+
+    a1.insert(9);
+    expect(a1).toEq(Z.A(9));
+    a1.insert(3);
+    expect(a1).toEq(Z.A(9, 3));
+    a1.insert(12);
+    expect(a1).toEq(Z.A(12, 9, 3));
+    a1.insert(2);
+    expect(a1).toEq(Z.A(12, 9, 3, 2));
+
+    a2.insert('the');
+    expect(a2).toEq(Z.A('the'));
+    a2.insert('quick');
+    expect(a2).toEq(Z.A('the', 'quick'));
+    a2.insert('brown');
+    expect(a2).toEq(Z.A('the', 'quick', 'brown'));
+    a2.insert('fox');
+    expect(a2).toEq(Z.A('the', 'quick', 'fox', 'brown'));
+  });
+});
+
+describe('Z.SortedArray constructor with `path` option', function() {
+  it('should insert items in sorted order as determined by comparing the values returned by getting the given path', function() {
+    var a1 = Z.SortedArray.create({path: 'bar'}),
+        a2 = Z.SortedArray.create({path: 'bar.x'});
+
+    a1.insert(Foo.create({bar: 17}));
+    expect(a1.pluck('bar')).toEq(Z.A(17));
+    a1.insert(Foo.create({bar: 14}));
+    expect(a1.pluck('bar')).toEq(Z.A(14, 17));
+    a1.insert(Foo.create({bar: 15}));
+    expect(a1.pluck('bar')).toEq(Z.A(14, 15, 17));
+    a1.insert(Foo.create({bar: 11}));
+    expect(a1.pluck('bar')).toEq(Z.A(11, 14, 15, 17));
+
+    a2.insert(Foo.create({bar: Bar.create({x: 17})}));
+    expect(a2.pluck('bar.x')).toEq(Z.A(17));
+    a2.insert(Foo.create({bar: Bar.create({x: 14})}));
+    expect(a2.pluck('bar.x')).toEq(Z.A(14, 17));
+    a2.insert(Foo.create({bar: Bar.create({x: 15})}));
+    expect(a2.pluck('bar.x')).toEq(Z.A(14, 15, 17));
+    a2.insert(Foo.create({bar: Bar.create({x: 11})}));
+    expect(a2.pluck('bar.x')).toEq(Z.A(11, 14, 15, 17));
   });
 
-  describe('given a comparison function', function() {
-    it('should set the comparison function to the given function', function() {
-      var f = function() {}, a = Z.SortedArray.create(f);
-      expect(a.cmp).toBe(f);
+  it("should adjust the order accordingly when an item's path changes", function() {
+    var a1 = Z.SortedArray.create({path: 'bar'}),
+        a2 = Z.SortedArray.create({path: 'bar.x'});
+
+    a1.insert(Foo.create({bar: 17}));
+    a1.insert(Foo.create({bar: 14}));
+    a1.insert(Foo.create({bar: 15}));
+    a1.insert(Foo.create({bar: 11}));
+
+    expect(a1.pluck('bar')).toEq(Z.A(11, 14, 15, 17));
+    a1.at(0).bar(21);
+    expect(a1.pluck('bar')).toEq(Z.A(14, 15, 17, 21));
+
+    a2.insert(Foo.create({bar: Bar.create({x: 17})}));
+    a2.insert(Foo.create({bar: Bar.create({x: 14})}));
+    a2.insert(Foo.create({bar: Bar.create({x: 15})}));
+    a2.insert(Foo.create({bar: Bar.create({x: 11})}));
+
+    expect(a2.pluck('bar.x')).toEq(Z.A(11, 14, 15, 17));
+    a2.set('last.bar.x', 12);
+    expect(a2.pluck('bar.x')).toEq(Z.A(11, 12, 14, 15));
+  });
+});
+
+describe('Z.SortedArray constructor with `path` and `isDescending` options', function() {
+  it('should insert items in sorted order as determined by comparing the values returned by getting the given path but in descending order', function() {
+    var a1 = Z.SortedArray.create({path: 'bar', isDescending: true}),
+        a2 = Z.SortedArray.create({path: 'bar.x', isDescending: true});
+
+    a1.insert(Foo.create({bar: 17}));
+    expect(a1.pluck('bar')).toEq(Z.A(17));
+    a1.insert(Foo.create({bar: 14}));
+    expect(a1.pluck('bar')).toEq(Z.A(17, 14));
+    a1.insert(Foo.create({bar: 15}));
+    expect(a1.pluck('bar')).toEq(Z.A(17, 15, 14));
+    a1.insert(Foo.create({bar: 11}));
+    expect(a1.pluck('bar')).toEq(Z.A(17, 15, 14, 11));
+
+    a2.insert(Foo.create({bar: Bar.create({x: 17})}));
+    expect(a2.pluck('bar.x')).toEq(Z.A(17));
+    a2.insert(Foo.create({bar: Bar.create({x: 14})}));
+    expect(a2.pluck('bar.x')).toEq(Z.A(17, 14));
+    a2.insert(Foo.create({bar: Bar.create({x: 15})}));
+    expect(a2.pluck('bar.x')).toEq(Z.A(17, 15, 14));
+    a2.insert(Foo.create({bar: Bar.create({x: 11})}));
+    expect(a2.pluck('bar.x')).toEq(Z.A(17, 15, 14, 11));
+  });
+
+  it("should adjust the order accordingly when an item's path changes", function() {
+    var a1 = Z.SortedArray.create({path: 'bar', isDescending: true}),
+        a2 = Z.SortedArray.create({path: 'bar.x', isDescending: true});
+
+    a1.insert(Foo.create({bar: 17}));
+    a1.insert(Foo.create({bar: 14}));
+    a1.insert(Foo.create({bar: 15}));
+    a1.insert(Foo.create({bar: 11}));
+
+    expect(a1.pluck('bar')).toEq(Z.A(17, 15, 14, 11));
+    a1.at(0).bar(13);
+    expect(a1.pluck('bar')).toEq(Z.A(15, 14, 13, 11));
+
+    a2.insert(Foo.create({bar: Bar.create({x: 17})}));
+    a2.insert(Foo.create({bar: Bar.create({x: 14})}));
+    a2.insert(Foo.create({bar: Bar.create({x: 15})}));
+    a2.insert(Foo.create({bar: Bar.create({x: 11})}));
+
+    expect(a2.pluck('bar.x')).toEq(Z.A(17, 15, 14, 11));
+    a2.set('last.bar.x', 16);
+    expect(a2.pluck('bar.x')).toEq(Z.A(17, 16, 15, 14));
+  });
+});
+
+describe('Z.SortedArray constructor with `compareFn` option', function() {
+  it('should insert items in sorted order as determined by the given function', function() {
+    var a = Z.SortedArray.create({
+      compareFn: function(a, b) {
+        return Z.cmp(b.get('bar.x'), a.get('bar.x'));
+      }
+    });
+
+    a.insert(Foo.create({bar: Bar.create({x: 17})}));
+    expect(a.pluck('bar.x')).toEq(Z.A(17));
+    a.insert(Foo.create({bar: Bar.create({x: 14})}));
+    expect(a.pluck('bar.x')).toEq(Z.A(17, 14));
+    a.insert(Foo.create({bar: Bar.create({x: 15})}));
+    expect(a.pluck('bar.x')).toEq(Z.A(17, 15, 14));
+    a.insert(Foo.create({bar: Bar.create({x: 11})}));
+    expect(a.pluck('bar.x')).toEq(Z.A(17, 15, 14, 11));
+  });
+
+  describe('and `dependsOn` option', function() {
+    it("should adjust the order accordingly when any of the dependent paths change", function() {
+      var a = Z.SortedArray.create({
+        dependsOn: ['bar.x'],
+        compareFn: function(a, b) {
+          return Z.cmp(b.get('bar.x'), a.get('bar.x'));
+        }
+      });
+
+      a.insert(Foo.create({bar: Bar.create({x: 17})}));
+      a.insert(Foo.create({bar: Bar.create({x: 14})}));
+      a.insert(Foo.create({bar: Bar.create({x: 15})}));
+      a.insert(Foo.create({bar: Bar.create({x: 11})}));
+
+      expect(a.pluck('bar.x')).toEq(Z.A(17, 15, 14, 11));
+      a.set('last.bar.x', 16);
+      expect(a.pluck('bar.x')).toEq(Z.A(17, 16, 15, 14));
     });
   });
 });
 
-describe('Z.SortedArray.insert', function() {
-  it('should maintain sorted order of the items as determined by the comparison function', function() {
-    var a1 = Z.SortedArray.create(),
-        a2 = Z.SortedArray.create(function(a, b) { return Z.cmp(b, a); });
+describe('Z.Array.index', function() {
+  var a = Z.SortedArray.create();
 
-    expect(a1.toArray()).toEq(Z.A());
-    expect(a2.toArray()).toEq(Z.A());
-    a1.insert(10);
-    a2.insert(10);
-    expect(a1.toArray()).toEq(Z.A(10));
-    expect(a2.toArray()).toEq(Z.A(10));
-    a1.insert(12);
-    a2.insert(12);
-    expect(a1.toArray()).toEq(Z.A(10, 12));
-    expect(a2.toArray()).toEq(Z.A(12, 10));
-    a1.insert(7);
-    a2.insert(7);
-    expect(a1.toArray()).toEq(Z.A(7, 10, 12));
-    expect(a2.toArray()).toEq(Z.A(12, 10, 7));
-    a1.insert(11);
-    a2.insert(11);
-    expect(a1.toArray()).toEq(Z.A(7, 10, 11, 12));
-    expect(a2.toArray()).toEq(Z.A(12, 11, 10, 7));
+  a.insert(5);
+  a.insert(21);
+  a.insert(17);
+  a.insert(111);
+
+  it('should return `null` if the given object is not in the array', function() {
+    expect(a.index(6)).toBeNull();
+    expect(a.index('foo')).toBeNull();
+    expect(a.index(null)).toBeNull();
+    expect(a.index({})).toBeNull();
+    expect(a.index(Z.Object.create())).toBeNull();
+  });
+
+  it('should return the current index of the item in the array', function() {
+    expect(a.index(5)).toBe(0);
+    expect(a.index(17)).toBe(1);
+    expect(a.index(21)).toBe(2);
+    expect(a.index(111)).toBe(3);
   });
 });
 
--- src/sorted_array.js
+++ src/sorted_array.js
@@ -3,16 +3,66 @@
 var slice = Array.prototype.slice;
 
 Z.SortedArray = Z.Array.extend(function() {
-  this.def('initialize', function(f) {
-    this.cmp = f || Z.cmp;
+  function dependentPathDidChange(n) {
+    this.remove(n.observee);
+    this.insert(n.observee);
+  }
+
+  this.def('initialize', function(opts) {
     this.supr();
+
+    opts = opts || {};
+
+    this.__z_paths__ = opts.dependsOn ? [].concat(opts.dependsOn) : [];
+
+    if (opts.path) {
+      this.__z_cmp__ = function(a, b) {
+        var r = Z.cmp(a.get(opts.path), b.get(opts.path));
+        return opts.isDescending ? r * -1 : r;
+      };
+
+      this.__z_paths__.push(opts.path);
+    }
+    else if (opts.compareFn) {
+      this.__z_cmp__ = opts.compareFn;
+    }
+    else if (opts.isDescending) {
+      this.__z_cmp__ = function(a, b) { return -1 * Z.cmp(a, b); };
+    }
+    else {
+      this.__z_cmp__ = Z.cmp;
+    }
+  });
+
+  this.def('splice', function(i, n) {
+    var size    = this.size(),
+        idx     = i < 0 ? size + i : i,
+        added   = slice.call(arguments, 2),
+        removed = n > 0 ? this.slice(idx, n).toNative() : [],
+        paths   = this.__z_paths__,
+        j, len, k, plen;
+
+
+    if (paths) {
+      for (j = 0, plen = paths.length; j < plen; j++) {
+        for (k = 0, len = added.length; k < len; k++) {
+          added[j].observe(paths[j], this, dependentPathDidChange);
+        }
+
+        for (k = 0, len = removed.length; k < len; k++) {
+          removed[j].stopObserving(paths[j], this, dependentPathDidChange);
+        }
+      }
+    }
+
+    return this.supr.apply(this, slice.call(arguments));
   });
 
   this.def('insert', function(o) {
-    var cmp = this.cmp, i, len;
+    var cmp = this.__z_cmp__, i, len;
 
     for (i = 0, len = this.size(); i < len; i++) {
-      if (cmp(this.at(i), o) === 1) { break; }
+      if (cmp(this.at(i), o) > 0) { break; }
     }
 
     return this.splice(i, 0, o);
