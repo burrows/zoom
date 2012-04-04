184a2a211f046742cef04bb692eac04e26470d7a
Corey Burrows <corey.burrows@gmail.com>
2012-04-04 09:00:42 -0500

    Z.Module now supports mixin in properties in addition to methods.

--- spec/module_spec.js
+++ spec/module_spec.js
@@ -5,6 +5,7 @@ var Z = this.Z || require('zoom'), TestMod;
 TestMod = Z.Module.create(function() {
   this.def('foo', function() {});
   this.def('bar', function() {});
+  this.property('baz');
 });
 
 describe('Z.Module.createMixin', function() {
@@ -17,6 +18,11 @@ describe('Z.Module.createMixin', function() {
     var mixin = TestMod.createMixin(Z.Object);
     expect(mixin.respondTo('foo')).toBe(true);
     expect(mixin.respondTo('bar')).toBe(true);
+  });
+
+  it('should copy all properties to the mixin object', function() {
+    var mixin = TestMod.createMixin(Z.Object);
+    expect(mixin.hasProperty('baz')).toBe(true);
   });
 });
 
@@ -34,6 +40,23 @@ describe('Z.Module.def', function() {
 
     expect(mixin1.respondTo('quux')).toBe(true);
     expect(mixin2.respondTo('quux')).toBe(true);
+  });
+});
+
+describe('Z.Module.property', function() {
+  it('should define the property on all of its mixin objects', function() {
+    var mixin1 = TestMod.createMixin(Z.Object),
+        mixin2 = TestMod.createMixin(Z.Object);
+
+    expect(mixin1.hasProperty('quux')).toBe(false);
+    expect(mixin2.hasProperty('quux')).toBe(false);
+
+    TestMod.open(function() {
+      this.property('quux');
+    });
+
+    expect(mixin1.hasProperty('quux')).toBe(true);
+    expect(mixin2.hasProperty('quux')).toBe(true);
   });
 });
 
--- src/module.js
+++ src/module.js
@@ -1,40 +1,61 @@
 (function(undefined) {
 
+var slice = Array.prototype.slice;
+
 Z.Module = Z.Object.extend(function() {
   this.def('initialize', function(f) {
     this.supr();
 
-    this.isPrototype  = true;
-    this.__z_mixins__ = [];
+    this.isPrototype      = true;
+    this.__z_mixins__     = [];
+    this.__z_defs__       = [];
+    this.__z_properties__ = [];
 
     if (f) { this.open(f); }
   });
 
   this.def('createMixin', function(prototype) {
-    var o = Object.create(prototype), k;
+    var o     = Object.create(prototype),
+        defs  = this.__z_defs__,
+        props = this.__z_properties__,
+        i, len;
 
     o.__z_module__ = this;
 
-    for (k in this) {
-      if (this.hasOwnProperty(k) && !k.match(/^__z_/)) { o[k] = this[k]; }
-    }
+    for (i = 0, len = defs.length; i < len; i++) { o.def.apply(o, defs[i]); }
+    for (i = 0, len = props.length; i < len; i++) { o.property.apply(o, props[i]); }
 
     this.__z_mixins__.push(o);
 
     return o;
   });
 
-  this.def('def', function(name, f) {
-    var r       = this.supr(name, f),
-        objects = this.__z_mixins__,
-        len     = objects.length,
+  this.def('property', function() {
+    var args   = slice.call(arguments),
+        r      = this.supr.apply(this, args),
+        mixins = this.__z_mixins__,
+        len    = mixins.length,
         i;
 
-    r = this.supr(name, f);
+    this.__z_properties__.push(args);
 
-    for (i = 0; i < len; i++) { objects[i].def(name, f); }
+    for (i = 0; i < len; i++) { mixins[i].property.apply(mixins[i], args); }
 
-    return f;
+    return r;
+  });
+
+  this.def('def', function() {
+    var args   = slice.call(arguments),
+        r      = this.supr.apply(this, args),
+        mixins = this.__z_mixins__,
+        len    = mixins.length,
+        i;
+
+    this.__z_defs__.push(args);
+
+    for (i = 0; i < len; i++) { mixins[i].def.apply(mixins[i], args); }
+
+    return r;
   });
 });
 
