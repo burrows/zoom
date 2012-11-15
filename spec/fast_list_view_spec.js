33630c9375236cee5a3a4514a89324fc0a173e6b
Corey Burrows <corey.burrows@gmail.com>
2012-11-15 11:02:17 -0600

    Adds remaining specs for Z.FastListView with custom row heights.

--- spec/fast_list_view_spec.js
+++ spec/fast_list_view_spec.js
@@ -75,7 +75,7 @@ describe('Z.FastListView', function() {
     });
 
     describe('.displayRange', function() {
-      it('should return the first and list index of the items to render based on the current scrollOffset and rowHeight', function() {
+      it('should return the first and list index of the items to render based on the current scrollOffset, rowHeight, and overflow properties', function() {
         expect(view.displayRange()).toEq([0, 8]);
       });
 
@@ -132,11 +132,31 @@ describe('Z.FastListView', function() {
         expect(view.displayRange()).toEq(null);
       });
     });
+
+    describe('.rowHeightFor', function() {
+      it('should return the rowHeight property', function() {
+        expect(view.rowHeightFor(0)).toBe(10);
+        expect(view.rowHeightFor(5)).toBe(10);
+        expect(view.rowHeightFor(25)).toBe(10);
+        expect(view.rowHeightFor(49)).toBe(10);
+      });
+    });
+
+    describe('.rowOffsetFor', function() {
+      it('should return the index times the rowHeight property', function() {
+        expect(view.rowOffsetFor(0)).toBe(0);
+        expect(view.rowOffsetFor(5)).toBe(50);
+        expect(view.rowOffsetFor(25)).toBe(250);
+        expect(view.rowOffsetFor(49)).toBe(490);
+      });
+    });
   });
 
   describe('with custom row heights', function() {
     beforeEach(function() {
-      view.def('customRowHeightFor', function(i) { return i * 10; });
+      view.def('customRowHeightFor', function(i) {
+        return i === 25 ? 5 : i * 10;
+      });
       view.customRowHeightIndexes().push(5, 25, 40);
     });
 
@@ -146,7 +166,7 @@ describe('Z.FastListView', function() {
 
     describe('.customRowHeights', function() {
       it('should return a native object mapping each custom index to its custom height', function() {
-        expect(view.customRowHeights()).toEq({5: 50, 25: 250, 40: 400});
+        expect(view.customRowHeights()).toEq({5: 50, 25: 5, 40: 400});
       });
     });
 
@@ -155,8 +175,8 @@ describe('Z.FastListView', function() {
         expect(view.offsetAdjustments()).toEq([
           [0,   5,   0],
           [6,  25,  40],
-          [26, 40, 280],
-          [41, 49, 670]
+          [26, 40,  35],
+          [41, 49, 425]
         ]);
       });
 
@@ -166,8 +186,8 @@ describe('Z.FastListView', function() {
         expect(view.offsetAdjustments()).toEq([
           [0,   5,   0],
           [6,  25,  40],
-          [26, 40, 280],
-          [41, 47, 670]
+          [26, 40,  35],
+          [41, 47, 425]
         ]);
       });
 
@@ -176,8 +196,8 @@ describe('Z.FastListView', function() {
         expect(view.offsetAdjustments()).toEq([
           [0,   5,   0],
           [6,  25,  30],
-          [26, 40, 260],
-          [41, 49, 640]
+          [26, 40,  15],
+          [41, 49, 395]
         ]);
       });
 
@@ -187,170 +207,128 @@ describe('Z.FastListView', function() {
           [0,   5,   0],
           [6,  10,  40],
           [11, 25, 130],
-          [26, 40, 370],
-          [41, 49, 760]
+          [26, 40, 125],
+          [41, 49, 515]
         ]);
       });
     });
 
     describe('.displayRange', function() {
+      it('should adjust the first and last indexes to account for the custom row heights', function() {
+        expect(view.displayRange()).toEq([0, 8]);
+        view.scrollOffset(60);
+        expect(view.displayRange()).toEq([3, 11]);
+        view.scrollOffset(290);
+        expect(view.displayRange()).toEq([23, 32]);
+      });
+    });
+
+    describe('.rowHeightFor', function() {
+      it('should return the rowHeight property for non-custom rows', function() {
+        expect(view.rowHeightFor(0)).toBe(10);
+        expect(view.rowHeightFor(49)).toBe(10);
+      });
+
+      it('should return the return value of `customRowHeightForIndex` for custom rows', function() {
+        expect(view.rowHeightFor(5)).toBe(50);
+        expect(view.rowHeightFor(25)).toBe(5);
+        expect(view.rowHeightFor(40)).toBe(400);
+      });
+    });
+
+    describe('.rowOffsetFor', function() {
+      it('should return the index times the rowHeight property plus the adjustment for the range the given index falls in', function() {
+        expect(view.rowOffsetFor(0)).toBe(0);
+        expect(view.rowOffsetFor(5)).toBe(50);
+        expect(view.rowOffsetFor(6)).toBe(60 + 40);
+        expect(view.rowOffsetFor(7)).toBe(70 + 40);
+        expect(view.rowOffsetFor(25)).toBe(250 + 40);
+        expect(view.rowOffsetFor(26)).toBe(260 + 35);
+        expect(view.rowOffsetFor(42)).toBe(420 + 425);
+      });
     });
   });
 
-  //it('should only add enough item views to fill the height of the view plus two times the number of overflow pixels', function() {
-  //  expect(view.get('subviews.size')).toBe(9); // 5 to fill the 50px height + 4 to fill 40 px of overflow
-  //});
+  describe('.update', function() {
+    it('should set the content of the item views based the indexes returned by the displayRange property', function() {
+      expect(view.subviews().at(0).content()).toBe(items.at(0));
+      expect(view.subviews().at(1).content()).toBe(items.at(1));
+      expect(view.subviews().at(2).content()).toBe(items.at(2));
+      expect(view.subviews().at(3).content()).toBe(items.at(3));
+      expect(view.subviews().at(4).content()).toBe(items.at(4));
+      expect(view.subviews().at(5).content()).toBe(items.at(5));
+      expect(view.subviews().at(6).content()).toBe(items.at(6));
+      expect(view.subviews().at(7).content()).toBe(items.at(7));
+      expect(view.subviews().at(8).content()).toBe(items.at(8));
 
-  //it('should set the content of the item views based on the `top` and `overflow` properties', function() {
-  //  expect(view.subviews().at(0).content()).toBe(items.at(0));
-  //  expect(view.subviews().at(1).content()).toBe(items.at(1));
-  //  expect(view.subviews().at(2).content()).toBe(items.at(2));
-  //  expect(view.subviews().at(3).content()).toBe(items.at(3));
-  //  expect(view.subviews().at(4).content()).toBe(items.at(4));
-  //  expect(view.subviews().at(5).content()).toBe(items.at(5));
-  //  expect(view.subviews().at(6).content()).toBe(items.at(6));
+      view.scrollOffset(40);
+      view.display();
 
-  //  view.top(20);
-  //  view.display();
+      expect(view.subviews().at(0).content()).toBe(items.at(9));
+      expect(view.subviews().at(1).content()).toBe(items.at(10));
+      expect(view.subviews().at(2).content()).toBe(items.at(2));
+      expect(view.subviews().at(3).content()).toBe(items.at(3));
+      expect(view.subviews().at(4).content()).toBe(items.at(4));
+      expect(view.subviews().at(5).content()).toBe(items.at(5));
+      expect(view.subviews().at(6).content()).toBe(items.at(6));
+      expect(view.subviews().at(7).content()).toBe(items.at(7));
+      expect(view.subviews().at(8).content()).toBe(items.at(8));
 
-  //  expect(view.subviews().at(0).content()).toBe(items.at(7));
-  //  expect(view.subviews().at(1).content()).toBe(items.at(1));
-  //  expect(view.subviews().at(2).content()).toBe(items.at(2));
-  //  expect(view.subviews().at(3).content()).toBe(items.at(3));
-  //  expect(view.subviews().at(4).content()).toBe(items.at(4));
-  //  expect(view.subviews().at(5).content()).toBe(items.at(5));
-  //  expect(view.subviews().at(6).content()).toBe(items.at(6));
+      view.scrollOffset(450);
+      view.display();
 
-  //  view.top(40);
-  //  view.display();
+      expect(view.subviews().at(0).content()).toBe(items.at(45));
+      expect(view.subviews().at(1).content()).toBe(items.at(46));
+      expect(view.subviews().at(2).content()).toBe(items.at(47));
+      expect(view.subviews().at(3).content()).toBe(items.at(48));
+      expect(view.subviews().at(4).content()).toBe(items.at(49));
+      expect(view.subviews().at(5).content()).toBe(items.at(41));
+      expect(view.subviews().at(6).content()).toBe(items.at(42));
+      expect(view.subviews().at(7).content()).toBe(items.at(43));
+      expect(view.subviews().at(8).content()).toBe(items.at(44));
+    });
 
-  //  expect(view.subviews().at(0).content()).toBe(items.at(7));
-  //  expect(view.subviews().at(1).content()).toBe(items.at(8));
-  //  expect(view.subviews().at(2).content()).toBe(items.at(9));
-  //  expect(view.subviews().at(3).content()).toBe(items.at(3));
-  //  expect(view.subviews().at(4).content()).toBe(items.at(4));
-  //  expect(view.subviews().at(5).content()).toBe(items.at(5));
-  //  expect(view.subviews().at(6).content()).toBe(items.at(6));
+    it('should set the `top` style properties of each item view based on the return value of rowOffsetFor', function() {
+      expect(view.subviews().at(0).node.style.top).toBe('0px');
+      expect(view.subviews().at(1).node.style.top).toBe('10px');
+      expect(view.subviews().at(2).node.style.top).toBe('20px');
+      expect(view.subviews().at(3).node.style.top).toBe('30px');
+      expect(view.subviews().at(4).node.style.top).toBe('40px');
+      expect(view.subviews().at(5).node.style.top).toBe('50px');
+      expect(view.subviews().at(6).node.style.top).toBe('60px');
+      expect(view.subviews().at(7).node.style.top).toBe('70px');
+      expect(view.subviews().at(8).node.style.top).toBe('80px');
 
-  //  view.top(40);
-  //  view.overflow(0);
-  //  view.display();
+      view.scrollOffset(450);
+      view.display();
 
-  //  expect(view.subviews().at(0).content()).toBe(items.at(5));
-  //  expect(view.subviews().at(1).content()).toBe(items.at(6));
-  //  expect(view.subviews().at(2).content()).toBe(items.at(7));
-  //  expect(view.subviews().at(3).content()).toBe(items.at(8));
-  //  expect(view.subviews().at(4).content()).toBe(items.at(4));
-  //});
+      expect(view.subviews().at(0).node.style.top).toBe('450px');
+      expect(view.subviews().at(1).node.style.top).toBe('460px');
+      expect(view.subviews().at(2).node.style.top).toBe('470px');
+      expect(view.subviews().at(3).node.style.top).toBe('480px');
+      expect(view.subviews().at(4).node.style.top).toBe('490px');
+      expect(view.subviews().at(5).node.style.top).toBe('410px');
+      expect(view.subviews().at(6).node.style.top).toBe('420px');
+      expect(view.subviews().at(7).node.style.top).toBe('430px');
+      expect(view.subviews().at(8).node.style.top).toBe('440px');
+    });
+  });
 
-  //it('should set the `top` style properties of each item view based on the `rowHeight` and content index', function() {
-  //  expect(view.subviews().at(0).node.style.top).toBe('0px');
-  //  expect(view.subviews().at(1).node.style.top).toBe('10px');
-  //  expect(view.subviews().at(2).node.style.top).toBe('20px');
-  //  expect(view.subviews().at(3).node.style.top).toBe('30px');
-  //  expect(view.subviews().at(4).node.style.top).toBe('40px');
-  //  expect(view.subviews().at(5).node.style.top).toBe('50px');
-  //  expect(view.subviews().at(6).node.style.top).toBe('60px');
+  describe('.scrollTo', function() {
+    describe('with a content item', function() {
+      it('should set the `scrollOffset` property to `rowHeight` times the index of the content item', function() {
+        view.scrollTo(items.at(40));
+        expect(view.scrollOffset()).toBe(400);
+      });
+    });
 
-  //  view.top(20);
-  //  view.display();
-
-  //  expect(view.subviews().at(0).node.style.top).toBe('70px');
-  //  expect(view.subviews().at(1).node.style.top).toBe('10px');
-  //  expect(view.subviews().at(2).node.style.top).toBe('20px');
-  //  expect(view.subviews().at(3).node.style.top).toBe('30px');
-  //  expect(view.subviews().at(4).node.style.top).toBe('40px');
-  //  expect(view.subviews().at(5).node.style.top).toBe('50px');
-  //  expect(view.subviews().at(6).node.style.top).toBe('60px');
-  //});
-
-  //describe('changing the content', function() {
-  //  it('should change the height of the container div', function() {
-  //    expect(view.node.childNodes[0].style.height).toBe('500px');
-  //    view.content(items.slice(0, 5));
-  //    view.display();
-  //    expect(view.node.childNodes[0].style.height).toBe('50px');
-  //  });
-
-  //  it('should change the content of the subviews', function() {
-  //    expect(view.subviews().at(0).content()).toBe(items.at(0));
-  //    expect(view.subviews().at(1).content()).toBe(items.at(1));
-  //    expect(view.subviews().at(2).content()).toBe(items.at(2));
-  //    expect(view.subviews().at(3).content()).toBe(items.at(3));
-  //    expect(view.subviews().at(4).content()).toBe(items.at(4));
-  //    expect(view.subviews().at(5).content()).toBe(items.at(5));
-  //    expect(view.subviews().at(6).content()).toBe(items.at(6));
-
-  //    view.content(items.slice(10, 10));
-  //    view.display();
-
-  //    expect(view.subviews().at(0).content()).toBe(items.at(10));
-  //    expect(view.subviews().at(1).content()).toBe(items.at(11));
-  //    expect(view.subviews().at(2).content()).toBe(items.at(12));
-  //    expect(view.subviews().at(3).content()).toBe(items.at(13));
-  //    expect(view.subviews().at(4).content()).toBe(items.at(14));
-  //    expect(view.subviews().at(5).content()).toBe(items.at(15));
-  //    expect(view.subviews().at(6).content()).toBe(items.at(16));
-  //  });
-
-  //  it('should remove subviews if there are no longer enough items to fill the height', function() {
-  //    expect(view.get('subviews.size')).toBe(7);
-  //    view.content(items.slice(10, 2));
-  //    view.display();
-  //    expect(view.get('subviews.size')).toBe(2);
-  //  });
-
-  //  it('should add subviews if necessary in order to fill the height when there are enough content items', function() {
-  //    view.content(items.slice(10, 2));
-  //    view.display();
-  //    expect(view.get('subviews.size')).toBe(2);
-  //    view.content(items.slice(10, 50));
-  //    view.display();
-  //    expect(view.get('subviews.size')).toBe(7);
-  //  });
-  //});
-
-  //describe('changing the `rowHeight` property', function() {
-  //  it('should change the height of the container div', function() {
-  //    expect(view.node.childNodes[0].style.height).toBe('500px');
-  //    view.rowHeight(20);
-  //    view.display();
-  //    expect(view.node.childNodes[0].style.height).toBe('1000px');
-  //  });
-
-  //  it('should change the number of subviews', function() {
-  //    expect(view.get('subviews.size')).toBe(7);
-  //    view.rowHeight(5);
-  //    view.display();
-  //    expect(view.get('subviews.size')).toBe(12); // 10 items to fill 50px + 2 overflow
-  //  });
-  //});
-
-  //describe('changing the `overflow` property', function() {
-  //  it('should change the number of subviews', function() {
-  //    expect(view.get('subviews.size')).toBe(7);
-  //    view.rowHeight(4);
-  //    view.display();
-  //    expect(view.get('subviews.size')).toBe(14); // 10 items to fill 50px + 4 overflow
-  //  });
-  //});
-
-  //describe('.scrollTo', function() {
-  //  describe('with a content item', function() {
-  //    it('should set the `top` property to `rowHeight` times the index of the content item', function() {
-  //      view.scrollTo(items.at(40));
-  //      expect(view.top()).toBe(400);
-  //    });
-  //  });
-
-  //  describe('with a number', function() {
-  //    it('should set the `top` property `rowHeight` times the given number', function() {
-  //      view.scrollTo(items.at(8));
-  //      expect(view.top()).toBe(80);
-  //    });
-  //  });
-  //});
-
+    describe('with a number', function() {
+      it('should set the `scrollOffset` property `rowHeight` times the given number', function() {
+        view.scrollTo(items.at(8));
+        expect(view.scrollOffset()).toBe(80);
+      });
+    });
+  });
 });
 
 }());
